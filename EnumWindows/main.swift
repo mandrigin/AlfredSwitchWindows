48773996d1ab8c89db233b167112ae28d6f88b80
Igor Mandrigin <igorm@opera.com>
2017-02-14 12:52:38 +0100

    Initial Commit

---
+++ EnumWindows/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  EnumWindows
+//
+//  Created by Igor Mandrigin on 2017-02-14.
+//  Copyright © 2017 Igor Mandrigin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
